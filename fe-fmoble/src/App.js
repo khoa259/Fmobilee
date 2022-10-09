4c111ed62db4f202b213bab6300084ccf5e927e7
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-10-10 01:14:07 +0700

    first commit

---
+++ fe-fmoble/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to Fmobile
+        
+      </h1>
+    </div>
+  );
+}
+
+export default App;
