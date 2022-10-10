5e3eeefc6f3070c5564d8121045bbe339b620ad3
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-10-10 09:35:04 +0700

    feat: code base

--- fe-fmoble/src/screens/homeScreen.js
+++ fe-fmoble/src/screens/homeScreen.js
@@ -1,20 +1,6 @@
 import React from "react";
-import Product from "../component/Product";
-import products from "../products";
-import { Row, Col } from "react-bootstrap";
 const HomeScreen = () => {
-  return (
-    <div>
-      <h1>Sản phẩm mới nhất</h1>
-      <Row>
-        {products.map((product, index) => (
-          <Col key={index} sm={12} md={6} lg={4}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HomeScreen;
