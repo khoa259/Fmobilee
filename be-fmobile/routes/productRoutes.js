4ade62aa27fe1888c474fbac1e3b8ed60743bccd
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-10-13 21:03:18 +0700

    BE-API-22: Fetching Products From DB

---
+++ be-fmobile/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import Product from "../model/productModel.js";
+// library
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+
+// @desc Fetch all Products
+// @route  GET/api/products
+// @access Public
+
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @desc Fetch single Product
+// @route  GET/api/products/:ud
+// @access Public
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params._id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({
+        message: "Products not found",
+      });
+    }
+    res.json(product);
+  })
+);
+
+export default router;
