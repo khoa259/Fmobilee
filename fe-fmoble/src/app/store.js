56d728f647019490d2f2200fec4a2ea76f6bede8
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-10-14 17:10:16 +0700

    WEB-26: Product List Reducer & Action

--- fe-fmoble/src/app/store.js
+++ fe-fmoble/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../slice/counterSlice";
+import { productListReducer } from "../reducer/productReducer";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: productListReducer,
   },
 });
