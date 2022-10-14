625c7c5e44953701cc01f8c8638447c03d639eb3
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-10-15 00:11:26 +0700

    feat: add module

--- be-fmobile/controllers/productController.js
+++ be-fmobile/controllers/productController.js
@@ -6,24 +6,24 @@ import asyncHandler from "express-async-handler";
 // @route  GET/api/products
 // @access Public
 
-const getProducts = asyncHandler (async (req, res) => {
-        const product = await Product.find({})
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
 
-        res.json(products)
-})
+  res.json(products);
+});
 
 // @desc Fetch single Product
 // @route  GET/api/products/:ud
 // @access Public
 
-const getProductsById = asyncHandler (async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-})
+const getProductsById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
 
-export{ getProducts, getProductsById }
+export { getProducts, getProductsById };
--- be-fmobile/routes/productRoutes.js
+++ be-fmobile/routes/productRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 // library
+import asyncHandler from "express-async-handler";
+import Product from "../model/productModel.js";
 const router = express.Router();
-import { getProducts, getProductsById } from '../controllers/productController.js'
+import {
+  getProducts,
+  getProductsById,
+} from "../controllers/productController.js";
 
-
-
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductsById)
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductsById);
 
 router.get(
   "/:id",
