2aab4801fce44bb2e7c2c7e2cbbd18f36e8092e7
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-10-21 21:41:32 +0700

    create redux

---
+++ fe-fmoble/src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "../reducers/userReducers";
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
