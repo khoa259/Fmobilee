d9f5997080901af779cb68bf7191a12ef49d865e
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-10-23 17:07:20 +0700

    feat: connect to db

--- be-fmobile/server.js
+++ be-fmobile/server.js
@@ -1,7 +1,28 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
 
 // app
 const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.json({ limit: "2mb" }));
+
+// connect DB
+mongoose
+  .connect("mongodb://localhost:27017/Fmobile")
+  .then(() => console.log("Connect DB success"))
+  .catch((err) => console.log("DB connect error", err));
+
+//route
+app.get("/api", (req, res) => {
+  res.json({
+    data: "hey you hit node API",
+  });
+});
 
 // port
 const port = process.env.PORT || 8000;
