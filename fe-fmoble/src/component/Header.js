45db298e3828a221188c71bee6282b834fdd47ba
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-10-27 14:13:47 +0700

    WEB-29 create navbar

--- fe-fmoble/src/component/Header.js
+++ fe-fmoble/src/component/Header.js
@@ -4,6 +4,7 @@ import { Badge, Container, Nav, Navbar } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
+import { MenuList } from "./data-menu/data-menu";
 const Header = () => {
   const history = useHistory();
   const dispatch = useDispatch();
@@ -19,46 +20,60 @@ const Header = () => {
 
   return (
     <div>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar variant="light" sticky="stickky-top">
         <Container>
           <Navbar.Brand href="/">Fmobile</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="/gio-hang">
-                <i className="px-1 fas fa-shopping-cart"></i>
-                {/* <div className="counter_cart">
+          <Nav as="ul" className="ml-auto mt-2">
+            <Nav.Link href="/gio-hang">
+              <i className="fas fa-shopping-cart"></i>
+              {/* <div className="counter_cart">
                   <span className="">0</span>
                 </div> */}
-                <Badge bg="dark ">0</Badge>
-              </Nav.Link>
-              <div className="dropdown">
-                {!user?.email && (
-                  <button className="dropbtn">
-                    <i className="px-1 fas fa-user"></i>
-                  </button>
-                )}
-                <div className="text-white pl-2 pt-3">
-                  {user?.email && user?.email.split("@")[0]}
-                </div>
-                {!user.email && (
-                  <div className="dropdown-content">
-                    <Link to="/register">đăng ký</Link>
-                    <Link to="/login">đăng nhập</Link>
-                  </div>
-                )}
-                {user.email && (
-                  <div className="dropdown-content">
-                    <Link to="/">đơn hàng</Link>
-                    <Link to="/" onClick={logout}>
-                      đăng xuất
-                    </Link>
-                  </div>
-                )}
+              <Badge bg="none">0</Badge>
+            </Nav.Link>
+            <div className="dropdown">
+              {!user?.email && (
+                <button className="dropbtn">
+                  <i className=" fas fa-user"></i>
+                </button>
+              )}
+              <div className="text-white pl-2 pt-3">
+                {user?.email && user?.email.split("@")[0]}
               </div>
-            </Nav>
-          </Navbar.Collapse>
+              {!user.email && (
+                <div className="dropdown-content">
+                  <Link to="/register">đăng ký</Link>
+                  <Link to="/login">đăng nhập</Link>
+                </div>
+              )}
+              {user.email && (
+                <div className="dropdown-content">
+                  <Link to="/">đơn hàng</Link>
+                  <Link to="/" onClick={logout}>
+                    đăng xuất
+                  </Link>
+                </div>
+              )}
+            </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </Nav>
         </Container>
+      </Navbar>
+      <Navbar
+        className="nav-child"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect="false">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="basic-navbar-nav justify-content-center ">
+          <Nav as="ul">
+            {MenuList.map((data, index) => (
+              <Nav.Item as="li" key={index}>
+                <Nav.Link href={data.path}>{data.label}</Nav.Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
