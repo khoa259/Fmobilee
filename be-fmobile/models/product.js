4497621ae4f483bb8d1d86a718109bc275a32e75
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-11-04 13:39:12 +0700

    BE-54 model Product

---
+++ be-fmobile/models/product.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      text: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+      text: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+    quantity: Number,
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ["Yes", "No"],
+    },
+    color: {
+      type: String,
+      enum: [
+        "Black",
+        "Gold",
+        "Silver",
+        "Pacific Blue",
+        "White",
+        "Green",
+        "Blue",
+        "Midnight ",
+      ],
+    },
+    brand: {
+      type: String,
+      enum: ["iphone", "macbook", "imac", "Apple Watch", "Phụ kiện"],
+    },
+    // ratings: [
+    //   {
+    //     star: Number,
+    //     postedBy: { type: ObjectId, ref: "User" },
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
