a52a662937c7da2ac35dae44e928dd18c72c4ebf
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-11-21 22:40:53 +0700

    WEB-94 Add Product To LocalStorage

---
+++ fe-fmoble/src/reducers/cartReducer.js
@@ -0,0 +1,13 @@
+const initialState = [];
+
+//load cart from localStrorage
+if(typeof window !== )
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return action.payload;
+    default:
+      return state;
+  }
+};
