1cf92666da72b5db6469271feae4e0aec46e74db
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-11-24 23:00:15 +0700

    [BE] Cart Models

--- be-fmobile/models/cart.js
+++ be-fmobile/models/cart.js
@@ -1,1 +1,25 @@
-import mongooo
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const cartSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: ObjectId,
+          ref: "Product",
+        },
+        count: {
+          type: Number,
+          color: String,
+          price: Number,
+        },
+      },
+    ],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderdBy: { type: ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("Cart", cartSchema);
