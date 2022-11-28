f579e84bbaf6be3e6ce621faba3d75d9dc2935c3
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-11-28 09:59:45 +0700

    remove components

--- fe-fmoble/src/pages/payments/Payments.js
+++ fe-fmoble/src/pages/payments/Payments.js
@@ -10,9 +10,7 @@ const Payments = () => {
     <div className="container p-5 text-center">
       <h4>Complete your purchase</h4>
       <Elements stripe={promise}>
-        <div className="col-md-8 offset-md-2">
-          <StripeCheckout />
-        </div>
+        <div className="col-md-8 offset-md-2">{/* <StripeCheckout /> */}</div>
       </Elements>
     </div>
   );
