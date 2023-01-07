0212d8da1d7877ffe4068860c267200dff2217ea
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2023-01-07 16:18:03 +0700

    [WEB] create footer & hover add to cart shop page

--- fe-fmoble/src/pages/cart/cart.js
+++ fe-fmoble/src/pages/cart/cart.js
@@ -71,32 +71,28 @@ const Cart = () => {
                     <div className="col-12 col-lg-6">
                       <span
                         className="mb-6 text-secondary"
-                        style={{ fontSize: 16 }}
-                      >
+                        style={{ fontSize: 16 }}>
                         Thông tin sản phẩm
                       </span>
                     </div>
                     <div className="col-12 col-lg-2">
                       <span
                         className="mb-6 text-secondary"
-                        style={{ fontSize: 16 }}
-                      >
+                        style={{ fontSize: 16 }}>
                         Giá tiền
                       </span>
                     </div>
                     <div className="col-12 col-lg-2 text-center">
                       <span
                         className="mb-6 text-secondary"
-                        style={{ fontSize: 16 }}
-                      >
+                        style={{ fontSize: 16 }}>
                         Số lượng
                       </span>
                     </div>
                     <div className="col-12 col-lg-2 text-end">
                       <span
                         className="mb-6 text-secondary"
-                        style={{ fontSize: 16 }}
-                      >
+                        style={{ fontSize: 16 }}>
                         Tổng
                       </span>
                     </div>
@@ -135,15 +131,13 @@ const Cart = () => {
                   <hr />
                   <div
                     className="d-flex justify-content-between
-                  "
-                  >
+                  ">
                     <h5>Tổng tiền:</h5>
                     <b className=" text-danger">{formatCash(`${total}`)}đ</b>
                   </div>
                   <button
                     className="btn btn-md btn-primary mt-3"
-                    onClick={redirectCheckout}
-                  >
+                    onClick={redirectCheckout}>
                     Thanh Toán
                   </button>
                 </div>
