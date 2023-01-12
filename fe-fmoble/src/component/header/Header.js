72250904d762cc79ca5fa1be2542a04cf5e38750
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2023-01-12 19:02:49 +0700

    [WEB] fix Navbar

--- fe-fmoble/src/component/header/Header.js
+++ fe-fmoble/src/component/header/Header.js
@@ -1,8 +1,8 @@
 import firebase from "firebase";
 import React, { useEffect, useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { Dropdown } from "antd";
 
 import { getCategories } from "../../functions/category";
 import { getUserCart } from "../../functions/user";
@@ -33,16 +33,66 @@ const Header = () => {
       setCartDB(e.data.products);
     });
   }, []);
+  const items = categories.map((c, i) => {
+    return {
+      key: i,
+      label: (
+        <Link
+          className="itemDrop"
+          // target="_blank"
+          rel="noopener noreferrer"
+          to={`/category/${c.slug}`}>
+          {c.name}
+        </Link>
+      ),
+    };
+  });
   return (
-    <div className="fixed-top ">
-      <Navbar variant="light">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/">Fmobile</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-xl" />
-          <Nav as="ul" className="ulHeader ml-auto mt-2 ">
+    <div className="header fixed-top">
+      <div className="header-item navbar">
+        <div className="logo">
+          <Link to="/">Fmobile</Link>
+        </div>
+        <div className="navItem">
+          <ul>
+            <li>
+              <Link className="navLink" to="/">
+                Trang Chủ
+              </Link>
+            </li>
+            <li>
+              <Dropdown
+                menu={{
+                  items,
+                }}
+                placement="bottom">
+                <Link className="navLink">
+                  Sản phẩm <i className="fa-solid fa-angle-down"></i>
+                </Link>
+              </Dropdown>
+            </li>
+            <li>
+              <Link className="navLink" to="/">
+                Tin Tức
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" to="/">
+                Liên Hệ
+              </Link>
+            </li>
+            <li>
+              <Link className="navLink" to="/">
+                Giới Thiệu
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="header-link-wrapper">
+          <div className="icon-search">
             <Search />
+          </div>
+          <div className="icon-bag">
             <Link to="/gio-hang">
               <i className="fas fa-shopping-cart position-relative">
                 <span className="badge" bg="none">
@@ -50,65 +100,44 @@ const Header = () => {
                 </span>
               </i>
             </Link>
-
-            <div className="dropdown">
-              {/* nếu user không tồn tai */}
-              {!user?.email && (
-                <button className="dropbtn">
-                  <i className=" fas fa-user"></i>
-                </button>
-              )}
-              <div className="text-white pl-2 ">
-                {user?.email && user?.email.split("@")[0]}
-              </div>
-              {!user?.email && (
-                <div className="dropdown-content">
-                  <Link to="/register">đăng ký</Link>
-                  <Link to="/login">đăng nhập</Link>
-                </div>
-              )}
-              {/* Nếu user có tồn tại  */}
-              {user?.email && user?.role === "subscriber" && (
-                <div className="dropdown-content">
-                  <Link to={`/user/profile/${user._id}`}>Settings</Link>
-                  <Link to="/" onClick={logout}>
-                    đăng xuất
-                  </Link>
-                </div>
-              )}
-
-              {user?.email && user?.role === "admin" && (
-                <div className="dropdown-content">
-                  <Link to="/" onClick={logout}>
-                    đăng xuất
-                  </Link>
-                  <Link to="/admin/dashboard">Dashboard</Link>
-                </div>
-              )}
+          </div>
+          <div className="dropdown">
+            {/* nếu user không tồn tai */}
+            {!user?.email && (
+              <button className="dropbtn">
+                <i className=" fas fa-user"></i>
+              </button>
+            )}
+            <div className="text-white pl-2 ">
+              {user?.email && user?.email.split("@")[0]}
             </div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          </Nav>
-        </Container>
-      </Navbar>
-      <Navbar
-        className="nav-child"
-        variant="dark"
-        expand="lg"
-        collapseOnSelect="false"
-      >
-        <Navbar.Toggle aria-controls="basic-navbar-nav " />
-        <Navbar.Collapse className="basic-navbar-nav justify-content-center">
-          <Nav as="ul" className="Ul">
-            {categories.map((c) => (
-              <Nav.Item as="li" className="LI" key={c._id}>
-                <Link className="navLink" to={`/category/${c.slug}`}>
-                  {c.name}
+            {!user?.email && (
+              <div className="dropdown-content">
+                <Link to="/register">đăng ký</Link>
+                <Link to="/login">đăng nhập</Link>
+              </div>
+            )}
+            {/* Nếu user có tồn tại  */}
+            {user?.email && user?.role === "subscriber" && (
+              <div className="dropdown-content">
+                <Link to={`/user/profile/${user._id}`}>Settings</Link>
+                <Link to="/" onClick={logout}>
+                  đăng xuất
                 </Link>
-              </Nav.Item>
-            ))}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+              </div>
+            )}
+
+            {user?.email && user?.role === "admin" && (
+              <div className="dropdown-content">
+                <Link to="/" onClick={logout}>
+                  đăng xuất
+                </Link>
+                <Link to="/admin/dashboard">Dashboard</Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
