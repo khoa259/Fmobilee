91df3d75a0ff933a9fc3a43ed4012b1ab4171f54
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2023-01-28 14:23:46 +0700

    [UI] bill details

--- fe-fmoble/src/pages/admin/order/OrderDetails.js
+++ fe-fmoble/src/pages/admin/order/OrderDetails.js
@@ -17,37 +17,77 @@ const OrderDetails = () => {
     };
     getProudct();
   }, [id]);
-  console.log("order", order.products);
+  console.log("order", order.username);
   // const { products } = order;
   // console.log("products", products?.price);
   return (
     <div>
       <h1 className="text-center">Chi tiết đơn hàng</h1>
-      {/* {JSON.stringify(order.products)} */}
-      <Descriptions title="User Info" bordered>
-        <Descriptions.Item label="Tên sản phẩm">
-          {order?.products?.map((p, i) => (
-            <div key={i}>
-              <span className="h5">{p.title}</span>
+      <div className="container border">
+        <div>
+          <div>
+            <div className="py-2">
+              <span className="h4">Họ tên: khoa10688</span>
             </div>
-          ))}
-          {/* {order.products.title} */}
-        </Descriptions.Item>
-        <Descriptions.Item label="Billing Mode">Prepaid</Descriptions.Item>
-        <Descriptions.Item label="Automatic Renewal">YES</Descriptions.Item>
-        <Descriptions.Item label="Thời gian đặt hàng">
-          <span className="h5">
-            {dateFormat(order.updatedAt, "dd/mm/yyyy - HH:MM:s")}
-          </span>
-        </Descriptions.Item>
-        <Descriptions.Item label="Tổng tiền" span={2}>
-          <span className="h5">{formatCash(`${order.billTotal}`)}đ</span>
-        </Descriptions.Item>
-        <Descriptions.Item label="Status" span={3}>
-          <Badge status="processing" text="Running" />
-        </Descriptions.Item>
-      </Descriptions>
-      <Button type="primary">In hóa đơn</Button>
+            <div className="py-2">
+              <span className="h4">
+                Địa Chỉ: số 12 ngõ 258/20 Hạ Đình, Thanh Xuân, Hà Nội{" "}
+              </span>
+            </div>
+            <div className="py-2 ">
+              <span className="h4">Email: khoa10688@gmail.com</span>
+            </div>
+            <div className="py-2 ">
+              <span className="h4">Số điện thoại:0964184106</span>
+            </div>
+          </div>
+          <div className="row pt-4">
+            <div className="col-4">
+              <span>Ngày đặt hàng: 15/01/2002</span>
+            </div>
+            <div className="col-4">
+              <span>
+                <span>Mã đơn hàng:554545484848</span>
+              </span>
+            </div>
+            <div className="col-4">
+              <span>Phương thức thanh toán: Payment</span>
+            </div>
+          </div>
+
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">STT</th>
+                <th scope="col">Tên Sản phẩm</th>
+                <th scope="col">Số lượng</th>
+                <th scope="col">Giá tiền</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="text-end row">
+            <span className="h4 col-11 ">Tổng tiền: 15.000.000 đ </span>
+          </div>
+          <div>
+            <Button type="primary">n hóa đơn</Button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
